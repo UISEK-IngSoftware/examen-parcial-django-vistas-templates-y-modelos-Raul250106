464610ea59bd8674a19a228ba558d8941d46c550
laboratorio.fs <jhelan.basantes@uisek.edu.ec>
2025-11-11 10:31:12 -0500

    Examen finalizado

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Movie (models.Model):
+    name = models.CharField(max_length=30, null=False)
+    gender = models.CharField(max_length=30, null=False)
+    director = models.CharField(max_length=30, null=False)
+    year = models.DateField()
+    sinopsis = models.TextField()
+    def __str__(self):
+        return self.name
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Movie
+from django.shortcuts import redirect, render
 
 # Create your views here.
+def index(request):
+    peliculas = Movie.objects.all()
+    template = loader.get_template('index.html')
+    context = {
+        'Peliculas': peliculas,        
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def Pelicula(request, movie_id):
+    Peliculas = Movie.objects.get(id = movie_id)
+    template = loader.get_template('display_movie.html')
+    context = {
+        'Peliculas': Peliculas,
+    }
+    return HttpResponse(template.render(context, request))
+
